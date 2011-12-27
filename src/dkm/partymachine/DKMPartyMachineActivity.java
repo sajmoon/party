01eec14ff74b97bbdfc27fe7c3e0112d4958e645
sajmoon <simon.strom@gmail.com>
2011-12-27 02:18:51 +0100

    init commit

---
+++ src/dkm/partymachine/DKMPartyMachineActivity.java
@@ -0,0 +1,18 @@
+package dkm.partymachine;
+
+import android.app.Activity; 
+import android.os.Bundle; 
+import android.widget.TextView; 
+
+public class DKMPartyMachineActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.main);
+            TextView tv = new TextView(this);
+            tv.setText("Hello, Android \n linuxconfig.org");
+            setContentView(tv);
+    }
+
+}
